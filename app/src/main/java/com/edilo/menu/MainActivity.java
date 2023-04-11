bab25944f72dc28d62c839d155109f66dd65238c
Edilo <edilo13@gmail.com>
2023-04-10 20:04:36 -0400

    Usando log cat

--- app/src/main/java/com/edilo/menu/MainActivity.java
+++ app/src/main/java/com/edilo/menu/MainActivity.java
@@ -3,15 +3,29 @@ package com.edilo.menu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String error = "Texto na variavel";
+        Button btn_qualquer = (Button) findViewById(R.id.btn);
+        btn_qualquer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Info", error);
+            }
+        });
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
